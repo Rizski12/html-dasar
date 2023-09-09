33a1e396fecd670c993df3c44c02ea2a2c5419b7
Rizski12 <rizkidianpratama784@gmail.com>
2023-09-09 12:59:44 +0700

    [FEAT] javascript fundamental

--- assets/js/index.js
+++ assets/js/index.js
@@ -90,33 +90,3 @@ let employee = {
 
 console.log(`Nama saya ${employee.nama} ,umur saya ${employee.umur} tahun, jurusan saya ${employee.jurusan}`)
 console.log(employee);
-
-// landing page
-let currentSlide = 0;
-const slides = document.querySelectorAll('.carousel-slide');
-
-function showSlide(n) {
-    if (n < 0) {
-        currentSlide = slides.length - 1;
-    } else if (n >= slides.length) {
-        currentSlide = 0;
-    }
-
-    for (let i = 0; i < slides.length; i++) {
-        slides[i].style.display = 'none';
-    }
-
-    slides[currentSlide].style.display = 'block';
-}
-
-function prevSlide() {
-    currentSlide--;
-    showSlide(currentSlide);
-}
-
-function nextSlide() {
-    currentSlide++;
-    showSlide(currentSlide);
-}
-
-showSlide(currentSlide);
